da8360ba5f1d905f8a51b0192560e7320c582057
zachbachiri <zachbachiri@gmail.com>
2014-07-31 16:59:28 -0400

    start mobile slideshow overlay/formatting

--- js/overlay.js
+++ js/overlay.js
@@ -6,60 +6,64 @@ $(document).ready(function(){
 	$("#slideshow-overlay3").hide(0);
 			
 
+		$('#show-hide1').click(function(){
+			event.preventDefault();
+			if ($(window).width() < 768){
+				$('#slideshow-overlay1').insertAfter('#show-hide1');
+			}
+			$("#slideshow-overlay1").slideToggle(0);
+			$("#slideshow-overlay2").hide(0);
+			$("#slideshow-overlay3").hide(0);
 
+			$(this).toggleClass('clicked');
+			$('#show-hide2').removeClass('clicked');
+			$('#show-hide3').removeClass('clicked');
 
+			$('#arrow1').toggleClass('uparrow');
+			$('#arrow2').removeClass('uparrow');
+			$('#arrow3').removeClass('uparrow');
+		});
+		
+				
+		$('#show-hide2').click(function(){
+			event.preventDefault();
+			if ($(window).width() < 768){
+				$('#slideshow-overlay2').insertAfter('#show-hide2');
+			}
+				$("#slideshow-overlay2").slideToggle(0);
+				$("#slideshow-overlay1").hide(0);
+				$("#slideshow-overlay3").hide(0);
 
-	$('#show-hide1').click(function(){
-		event.preventDefault();
-		$("#slideshow-overlay1").slideToggle(0);
-		$("#slideshow-overlay2").hide(0);
-		$("#slideshow-overlay3").hide(0);
+				$(this).toggleClass('clicked');
+			    $('#show-hide1').removeClass('clicked');
+			    $('#show-hide3').removeClass('clicked');
 
-		$(this).toggleClass('clicked');
-	    $('#show-hide2').removeClass('clicked');
-	    $('#show-hide3').removeClass('clicked');
+			    $('#arrow2').toggleClass('uparrow');
+			    $('#arrow1').removeClass('uparrow');
+			    $('#arrow3').removeClass('uparrow');
+		});
 
-	    $('#arrow1').toggleClass('uparrow');
-	    $('#arrow2').removeClass('uparrow');
-	    $('#arrow3').removeClass('uparrow');
-	});
+				
 
+				
+		$('#show-hide3').click(function(){
+			event.preventDefault();
+			if ($(window).width() < 768){
+				$('#slideshow-overlay3').insertAfter('#show-hide3');
+			}
+				$("#slideshow-overlay3").slideToggle(0);
+				$("#slideshow-overlay2").hide(0);
+				$("#slideshow-overlay1").hide(0);
 
+				$(this).toggleClass('clicked');
+			    $('#show-hide2').removeClass('clicked');
+			    $('#show-hide1').removeClass('clicked');
 
-			
-	$('#show-hide2').click(function(){
-		event.preventDefault();
-		$("#slideshow-overlay2").slideToggle(0);
-		$("#slideshow-overlay1").hide(0);
-		$("#slideshow-overlay3").hide(0);
-
-		$(this).toggleClass('clicked');
-	    $('#show-hide1').removeClass('clicked');
-	    $('#show-hide3').removeClass('clicked');
-
-	    $('#arrow2').toggleClass('uparrow');
-	    $('#arrow1').removeClass('uparrow');
-	    $('#arrow3').removeClass('uparrow');
-	});
-
-			
-
-			
-	$('#show-hide3').click(function(){
-		event.preventDefault();
-		$("#slideshow-overlay3").slideToggle(0);
-		$("#slideshow-overlay2").hide(0);
-		$("#slideshow-overlay1").hide(0);
-
-		$(this).toggleClass('clicked');
-	    $('#show-hide2').removeClass('clicked');
-	    $('#show-hide1').removeClass('clicked');
-
-	    $('#arrow3').toggleClass('uparrow');
-	    $('#arrow2').removeClass('uparrow');
-	    $('#arrow1').removeClass('uparrow');
-	});
-
+			    $('#arrow3').toggleClass('uparrow');
+			    $('#arrow2').removeClass('uparrow');
+			    $('#arrow1').removeClass('uparrow');
+		});
+		
 
 
 	
