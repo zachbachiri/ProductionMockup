69ec252ab995bc6c224634a6d6ec71b1845b108c
zachbachiri <zachbachiri@gmail.com>
2014-08-01 13:07:29 -0400

    mostly functional mobile overlays

--- js/overlay.js
+++ js/overlay.js
@@ -9,7 +9,13 @@ $(document).ready(function(){
 		$('#show-hide1').click(function(){
 			event.preventDefault();
 			if ($(window).width() < 768){
-				$('#slideshow-overlay1').insertAfter('#show-hide1');
+				$('#slideshow-overlay1').insertAfter('#arrow1');
+				$('#slideshow-link1').toggleClass("active");
+				$('#slideshow-link2').removeClass("active");
+				$('#slideshow-link3').removeClass("active");
+			} else {
+				$('#slideshow-overlay1').insertAfter('#final-link-wrap');
+				$('.slideshow-link').removeClass('active');
 			}
 			$("#slideshow-overlay1").slideToggle(0);
 			$("#slideshow-overlay2").hide(0);
@@ -28,7 +34,13 @@ $(document).ready(function(){
 		$('#show-hide2').click(function(){
 			event.preventDefault();
 			if ($(window).width() < 768){
-				$('#slideshow-overlay2').insertAfter('#show-hide2');
+				$('#slideshow-overlay2').insertAfter('#arrow2');
+				$('#slideshow-link2').toggleClass("active");
+				$('#slideshow-link1').removeClass("active");
+				$('#slideshow-link3').removeClass("active");
+			} else {
+				$('#slideshow-overlay2').insertAfter('#final-link-wrap');
+				$('.slideshow-link').removeClass('active');
 			}
 				$("#slideshow-overlay2").slideToggle(0);
 				$("#slideshow-overlay1").hide(0);
@@ -49,7 +61,13 @@ $(document).ready(function(){
 		$('#show-hide3').click(function(){
 			event.preventDefault();
 			if ($(window).width() < 768){
-				$('#slideshow-overlay3').insertAfter('#show-hide3');
+				$('#slideshow-overlay3').insertAfter('#arrow3');
+				$('#slideshow-link3').toggleClass("active");
+				$('#slideshow-link2').removeClass("active");
+				$('#slideshow-link1').removeClass("active");
+			} else {
+				$('#slideshow-overlay3').insertAfter('#final-link-wrap');
+				$('.slideshow-link').removeClass('active');
 			}
 				$("#slideshow-overlay3").slideToggle(0);
 				$("#slideshow-overlay2").hide(0);
