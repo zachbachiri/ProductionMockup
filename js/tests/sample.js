5add3ad2d7b02f3373e9d78061173ad0b14583c8
zachbachiri <zachbachiri@gmail.com>
2014-08-13 12:33:11 -0400

    things

--- js/tests/sample.js
+++ js/tests/sample.js
@@ -1,3 +1,4 @@
+//Sample CasperJS tests
 
 casper.test.begin("Hello, Test!", 2, function(test) {
 	casper.start('file:///Users/zachariabachiri/Desktop/Placester/Zach_ProductionTest/index.html', function() {
